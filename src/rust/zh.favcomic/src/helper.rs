6ab7e69299fbf796830222f7d37274e68e3a527a
WhisperKit <qiyeni@gmail.com>
2024-05-04 20:13:38 +0800

    add zh.favcomic

---
+++ src/rust/zh.favcomic/src/helper.rs
@@ -0,0 +1,107 @@
+extern crate alloc;
+
+use aidoku::{
+    error::{AidokuError},
+    prelude::*,
+    std::{
+        current_date,
+        defaults::{defaults_get, defaults_set},
+        net::{HttpMethod, Request},
+        String,
+    },
+};
+use alloc::string::ToString;
+use aidoku::std::html::Node;
+
+const WWW_URL: &str = "https://www.favcomic.com";
+
+pub fn gen_request(url: String, method: HttpMethod) -> Request {
+    let username = defaults_get("username").and_then(|v| v.as_string().map(|v| v.read())).unwrap_or_default();
+    let password = defaults_get("password").and_then(|v| v.as_string().map(|v| v.read())).unwrap_or_default();
+    let token = defaults_get("token").and_then(|v| v.as_string().map(|v| v.read())).unwrap_or_default();
+    let login_time = defaults_get("login_time").unwrap().as_int().unwrap_or_default();
+    let expires_i64: i64 = login_time + 604800;
+    let now_time_i64: i64 = gen_time().parse().unwrap_or(0);
+    // 有账号密码，但令牌或登录时间为空，或登录时间过期时重新登录
+    let cookie = if !url.contains("login") &&
+        !username.is_empty() && !password.is_empty() &&
+        ( (token.is_empty() || login_time == -1) || (expires_i64 < now_time_i64) ) {
+        login().unwrap()
+    } else {
+        token
+    };
+   Request::new(url, method)
+        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+        .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+       .header("Cookie", format!("token={}", cookie).as_str())
+}
+
+pub fn login() -> Result<String, AidokuError> {
+    let request = gen_request(gen_login_url(), HttpMethod::Post).header("Cookie", "");
+    let username = defaults_get("username")?.as_string()?.read();
+    let password = defaults_get("password")?.as_string()?.read();
+    if username.is_empty() || password.is_empty() {
+        return Ok("".to_string())
+    }
+    let body = format!("loginName={}&password={}", username, password);
+    let request = request.body(body.as_bytes());
+    request.send();
+    if request.status_code() != 200 {
+        return Ok("".to_string())
+    }
+    let json = request.json()?;
+    let data = json.as_object()?;
+    // 判断是否登录失败
+    let result = data.get("result").as_string()?.read();
+    if result == "fail" {
+        return Ok("".to_string())
+    }
+    let data = data.get("model").as_object()?;
+    let token = data.get("token");
+    let login_time = data.get("loginTime");
+    defaults_set("token", token.clone());
+    defaults_set("login_time", login_time.clone());
+    Ok(token.as_string()?.read())
+}
+
+pub fn gen_login_url() -> String {
+    format!("{}/{}", WWW_URL, "login")
+}
+
+pub fn gen_time() -> String {
+    format!("{}000", (current_date() as i64).to_string())
+}
+
+pub fn get_html(url: String) -> Result<Node, AidokuError> {
+    let request = gen_request(url, HttpMethod::Get);
+    request.send();
+    request.html()
+}
+
+pub fn gen_cate_url(cate_id: String, keyword: String, origin: String, finished: String, free: String, sort: String, page: String) -> String {
+    format!("{}/{}?keyword={}&origin={}&finished={}&free={}&tag=0&sort={}&page={}",
+            WWW_URL,
+            cate_id,
+            keyword,
+            origin,
+            finished,
+            free,
+            sort,
+            page
+    )
+}
+
+pub fn gen_detail_url(id: String) -> String {
+    format!("{}/comic/detail/{}",
+            WWW_URL,
+            id,
+    )
+}
+
+pub fn gen_chapter_url(id: String) -> String {
+    format!("{}/comic/chapter/{}",
+            WWW_URL,
+            id,
+    )
+}
---
+++ src/rust/zh.favcomic/src/lib.rs
@@ -0,0 +1,213 @@
+#![no_std]
+extern crate alloc;
+
+use aidoku::{error::Result, prelude::*, std::{String, Vec}, Chapter, Filter, FilterType, Manga, MangaPageResult, Page, MangaStatus, MangaContentRating, MangaViewer};
+use alloc::string::ToString;
+use aidoku::helpers::uri::encode_uri;
+
+mod helper;
+
+const FILTER_TYPES: [&str; 4] = ["boy", "girl", "r18", "picture"];
+const FILTER_ORIGIN: [&str; 5] = ["0", "2", "3", "1", "4"];
+const FILTER_FINISHED: [&str; 3] = ["0", "1", "2"];
+const FILTER_FREE: [&str; 3] = ["0", "1", "2"];
+const FILTER_SORT: [&str; 2] = ["2", "1"];
+
+#[get_manga_list]
+fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+	let mut query = String::new();
+	let mut types = String::new();
+	let mut origin = String::new();
+	let mut finished = String::new();
+	let mut free = String::new();
+	let mut sort = String::new();
+
+	for filter in filters {
+		match filter.kind {
+			FilterType::Title => {
+				query = filter.value.as_string()?.read();
+			}
+			FilterType::Select => {
+				let index = filter.value.as_int()? as usize;
+				match filter.name.as_str() {
+					"类型" => {
+						types = FILTER_TYPES[index].to_string();
+					}
+					"地区" => {
+						origin = FILTER_ORIGIN[index].to_string();
+					}
+					"进度" => {
+						finished = FILTER_FINISHED[index].to_string();
+					}
+					"付费" => {
+						free = FILTER_FREE[index].to_string();
+					}
+					"排序" => {
+						sort = FILTER_SORT[index].to_string();
+					}
+					_ => continue,
+				}
+			}
+			_ => continue,
+		}
+	}
+
+	let url = helper::gen_cate_url(types, encode_uri(query.clone()), origin, finished, free, sort, page.to_string());
+
+	let html = helper::get_html(url)?;
+
+	let has_more = query.is_empty();
+
+	let mut mangas: Vec<Manga> = Vec::new();
+
+	for item in html.select(".comic_li").array() {
+		let item = match item.as_node() {
+			Ok(node) => node,
+			Err(_) => continue,
+		};
+		let id = item
+			.select(".cover_box > a")
+			.attr("href")
+			.read().replace("/comic/detail/", "");
+		let cover = item.select(".cover_box > a > img").attr("src").read();
+		let title = item.select(".brief_box > div.title > a").attr("title").read().trim().to_string();
+		mangas.push(Manga {
+			id,
+			cover,
+			title,
+			..Default::default()
+		});
+	}
+
+	Ok(MangaPageResult {
+		manga: mangas,
+		has_more,
+	})
+}
+
+#[get_manga_details]
+fn get_manga_details(id: String) -> Result<Manga> {
+	let url = helper::gen_detail_url(id.clone());
+	let html = helper::get_html(url.clone())?;
+	let cover = html.select(".comic_cover")
+		.attr("src")
+		.read();
+	let title = html
+		.select(".title > h1")
+		.text().read();
+	let author = html
+		.select(".toggle_box > .author > a")
+		.array()
+		.map(|val| val.as_node().expect("Failed to get author").text().read())
+		.collect::<Vec<String>>()
+		.join(", ");
+	let artist = String::new();
+	let description = html
+		.select(".intro_box > h2")
+		.text()
+		.read()
+		.trim()
+		.to_string().replace("作品介绍：", "");
+	let categories = html
+		.select(".tag_box > a")
+		.array()
+		.map(|a| a.as_node().unwrap().text().read().trim().to_string())
+		.filter(|a| !a.is_empty())
+		.collect::<Vec<String>>();
+	let status = {
+		let status_text = html
+			.select(".state_box > span:nth-child(3)")
+			.text()
+			.read()
+			.trim()
+			.to_string();
+		if status_text.contains("完结") {
+			MangaStatus::Completed
+		} else if status_text.contains("连载中") {
+			MangaStatus::Ongoing
+		} else {
+			MangaStatus::Unknown
+		}
+	};
+	let nsfw = MangaContentRating::Safe;
+	let viewer = MangaViewer::Scroll;
+
+	Ok(Manga {
+		id,
+		cover,
+		title,
+		author,
+		artist,
+		description,
+		url,
+		categories,
+		status,
+		nsfw,
+		viewer,
+	})
+}
+
+#[get_chapter_list]
+fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
+	let url = helper::gen_detail_url(manga_id.clone());
+	let html = helper::get_html(url.clone())?;
+	let mut chapters: Vec<Chapter> = Vec::new();
+	for (index, item) in html.select(".item_box").array().enumerate() {
+		let item = match item.as_node() {
+			Ok(item) => item,
+			Err(_) => continue,
+		};
+		let id = item
+			.select("a")
+			.attr("href")
+			.read().replace("/comic/chapter/", "");
+		if id.is_empty() {
+			continue
+		}
+		let title = item.select(".title").text().read().trim().to_string();
+		let mut scanlator = item.select("span:nth-child(2)").text().read().trim().to_string().replace("￥", "").replace("&nbsp;", "");
+		if scanlator == "会员专享" {
+			scanlator = "会员免费".to_string();
+		} else {
+			scanlator = format!("￥ {}", scanlator );
+		}
+		let chapter = (index + 1) as f32;
+		let url = helper::gen_chapter_url(id.clone());
+		chapters.push(Chapter {
+			id,
+			title,
+			chapter,
+			scanlator,
+			url,
+			..Default::default()
+		});
+	}
+	chapters.reverse();
+	Ok(chapters)
+}
+
+#[get_page_list]
+fn get_page_list(_: String, chapter_id: String) -> Result<Vec<Page>> {
+	let url = helper::gen_chapter_url(chapter_id.clone());
+	let html = helper::get_html(url.clone())?;
+	let mut pages: Vec<Page> = Vec::new();
+
+	for (index, item) in html.select(".unlazyload").array().enumerate() {
+		let item = match item.as_node() {
+			Ok(node) => node,
+			Err(_) => continue,
+		};
+		let index = index as i32;
+		let mut url = item.attr("data-src").read().trim().to_string();
+		if url.is_empty() {
+			url = item.attr("src").read().trim().to_string();
+		}
+		pages.push(Page {
+			index,
+			url,
+			..Default::default()
+		})
+	}
+
+	Ok(pages)
+}
